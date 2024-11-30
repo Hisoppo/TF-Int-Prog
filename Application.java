c970509a83450bffb18777bb15f6ccb9c496ec74
Henrique Isoppo <h.isoppo@edu.pucrs.br>
2024-11-30 20:44:06 -0300

    Commit alterando a classe Passeio com a adição dos metódos passeio, mostra passeio, adiciona passageiro. Também altera a aplication, aplicando algumas sugestões do VSCode/Copilot para analisarmos.

--- Application.java
+++ Application.java
@@ -18,7 +18,7 @@ public class Application {
       CadastroBarcos cadastroBarcos = new CadastroBarcos();
       
       
-      Scanner sc = new Scanner(System.in); 
+      try (Scanner sc = new Scanner(System.in)) {
       Barco barco = new Barco(0, "", 0);
       Passageiro passageiro = new Passageiro("", 0, "");
       Passeio passeio = new Passeio(barco, 01);
@@ -28,56 +28,46 @@ public class Application {
       while(seletor != 11) {
 
          System.out.println("Selecione uma das opções:");
-         System.out.println("1 – Incluir barco \n"+
-                           " 2 – Remover barco \n " +
-                           " 3 – Mostrar barcos \n " +
-                           " 4 – Pesquisar e mostrar barco por nome \n "+
-                           " 5 – Criar passeio \n" + 
-                           " 6 – Mostrar passeio \n" +
-                           " 7 – Adicionar passageiro no passeio \n"+
-                           " 8 – Remover passageiro do passeio \n "+
-                           " 9 – Quantidade de passageiros por profissão \n"+
-                           " 10 – Valor total arrecadado pelo passeio \n"+
-                           " 11 - Sair do programa \n" ); 
+         System.out.println("""
+            1 – Incluir barco
+            2 – Remover barco
+            3 – Mostrar barcos
+            4 – Pesquisar e mostrar barco por nome
+            5 – Criar passeio
+            6 – Mostrar passeio
+            7 – Adicionar passageiro no passeio
+            8 – Remover passageiro do passeio
+            9 – Quantidade de passageiros por profissão
+            10 – Valor total arrecadado pelo passeio
+            11 - Sair do programa
+            """);
                            seletor = sc.nextInt();
                            sc.nextLine();
                            
          
          switch (seletor) {
-            case 1:
-               cadastroBarcos.adicionaBarco();
-               break;
-            case 2:
-               cadastroBarcos.removeBarco(barco.getNome());
-               break;
-            case 3:
-               cadastroBarcos.mostraBarcos();
-               break;
-            case 4:
-               cadastroBarcos.buscaBarcoNome(barco.getNome());
-               break;
-            case 5:
-               passeio.criaPasseio(barco, seletor);
-               break;
-            case 6:
-               passeio.mostraPasseio();
-               break;
-            case 7:
-               passeio.adicionaPassageiro(passageiro);
-               break;
-            case 8:
-               passeio.removePassageiro(passageiro.getNome());
-               break;
-            case 9:
-               passeio.buscaPasProfissao(passageiro.getProfissao());
-               break;
-            case 10:
-               passeio.buscaValorPasseio();
-               break;
-            case 11:
-               System.out.println("Encerrando o programa... Ate logo!");
-               break;
-         }
+            case 1 -> cadastroBarcos.adicionaBarco();
+            case 2 -> cadastroBarcos.removeBarco(barco.getNome());
+            case 3 -> cadastroBarcos.mostraBarcos();
+            case 4 -> cadastroBarcos.buscaBarcoNome(barco.getNome());
+            case 5 -> passeio.criaPasseio(barco, seletor);
+            case 6 -> passeio.mostraPasseio();
+            case 7 -> {
+               System.out.println("Digite o nome do passageiro:");
+               String nome = sc.nextLine();
+               System.out.println("Digite a idade do passageiro:");
+               int idade = sc.nextInt();
+               sc.nextLine(); // consume the newline
+               System.out.println("Digite a profissão do passageiro:");
+               String profissao = sc.nextLine();
+               passeio.adicionaPassageiro(nome, idade, profissao);
             }
+            case 8 -> passeio.removePassageiro(passageiro.getNome());
+            case 9 -> passeio.buscaPasProfissao(passageiro.getProfissao());
+            case 10 -> passeio.buscaValorPasseio();
+            case 11 -> System.out.println("Encerrando o programa... Ate logo!");
+            }
+         }
+      }
    }
 }
